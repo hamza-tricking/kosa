a04e885107c28730b3ac3aba5109f10b22e716d0
Mohamed <hamzatricks@gmail.com>
2026-04-10 00:28:45 +0100

    first

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://dmtart.pro/kosa'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Connect to MongoDB
