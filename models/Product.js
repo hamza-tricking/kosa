8e4c75fdbd267e1a2b1d89500ad12a80d4ef134c
Mohamed <hamzatricks@gmail.com>
2026-04-10 01:31:35 +0100

    first

--- models/Product.js
+++ models/Product.js
@@ -47,8 +47,7 @@ const productSchema = new mongoose.Schema({
     }
   }
 }, {
-  timestamps: true,
-  suppressReservedKeysWarning: true
+  timestamps: true
 });
 
 module.exports = mongoose.model('Product', productSchema);
