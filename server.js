07965faa04a24b70bbe579443f423da6cf78c607
Mohamed <hamzatricks@gmail.com>
2026-04-10 01:13:51 +0100

    first

--- server.js
+++ server.js
@@ -16,9 +16,7 @@ app.use(cors({
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  suppressReservedKeysWarning: true
-})
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log('MongoDB connection error:', err));
 
